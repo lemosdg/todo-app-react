e7b01cee3d5815f590c28929aa373627c0bc78c7
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-01 14:38:40 -0600

    Styles for dark mode on ButtonClear component

--- src/components/ButtonClear/index.jsx
+++ src/components/ButtonClear/index.jsx
@@ -1,13 +1,20 @@
 import { useCleaner } from "../../hooks/useCleaner";
+import { useScheme } from "../../hooks/useScheme";
 import "./index.css";
 
 export const ButtonClear = () => {
+  // Custom hooks
   const { isCleaning, clearTasks } = useCleaner();
+  const { scheme } = useScheme();
 
   return (
     <button
       onClick={clearTasks}
-      className={isCleaning ? "buttonClear not_allowed" : "buttonClear"}
+      className={
+        isCleaning
+          ? `buttonClear not_allowed ${scheme}`
+          : `buttonClear ${scheme}`
+      }
       disabled={isCleaning}
     >
       {isCleaning ? "Cleaning" : "Clear Completed"}
