9a1944024d668207fa5cee4b573d8ce875c3c6d0
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-01 14:38:01 -0600

    Styles for dark mode on FilterTasks component

--- src/components/FilterTasks/index.jsx
+++ src/components/FilterTasks/index.jsx
@@ -1,14 +1,18 @@
 import { FilterTasksButton } from "../FilterTasksButton";
+
 import { useFilterTasks } from "../../hooks/useFilterTasks";
+import { useScheme } from "../../hooks/useScheme";
 import "./index.css";
 
 export const FilterTasks = () => {
+  // Custom hooks
   const { all, active, completed, filterAll, filterActive, filterCompleted } =
     useFilterTasks();
+  const { scheme } = useScheme();
 
   return (
-    <section className="filter_tasks">
-      <div className="filter_tasks_wrapper">
+    <section className={`filter_tasks ${scheme}`}>
+      <div className={`filter_tasks_wrapper ${scheme}`}>
         <FilterTasksButton status={all} displayText="All" onClick={filterAll} />
         <FilterTasksButton
           status={active}
