929abd70059887a06125f20b8d104c037ba83fd0
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-01 14:37:25 -0600

    Styles for dark mode on FilterTaskButton component

--- src/components/FilterTasksButton/index.jsx
+++ src/components/FilterTasksButton/index.jsx
@@ -1,11 +1,17 @@
+import { useScheme } from "../../hooks/useScheme";
 import "./index.css";
 
 export const FilterTasksButton = ({ status, displayText, onClick }) => {
+  // Custom hooks
+  const { scheme } = useScheme();
+
   return (
     <button
       onClick={onClick}
       className={
-        status ? "filter_tasks_button button_active" : "filter_tasks_button"
+        status
+          ? `filter_tasks_button button_active ${scheme}`
+          : `filter_tasks_button ${scheme}`
       }
     >
       {displayText}
