7fbb9b4303855a5386bd11c774ffa1b0a650f4d5
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-01 14:38:24 -0600

    Styles for dark mode on ButtonDone component

--- src/components/ButtonDone/index.jsx
+++ src/components/ButtonDone/index.jsx
@@ -1,11 +1,16 @@
+import { useScheme } from "../../hooks/useScheme";
 import iconCheck from "../../assets/icon-check.svg";
 import "./index.css";
 
 export const ButtonDone = ({ done, onClick }) => {
+  const { scheme } = useScheme();
+
   return (
     <button
       type="button"
-      className={done ? "button_done active" : "button_done"}
+      className={
+        done ? `button_done active ${scheme}` : `button_done ${scheme}`
+      }
       onClick={onClick}
     >
       {done ? <img src={iconCheck} alt="Task was done" /> : ""}
