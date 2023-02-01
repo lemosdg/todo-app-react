1b31ae4c96aafb76b2574933c822da2f6f868d37
carlosqdev <carlosquintero95nic@gmail.com>
2023-02-01 10:37:15 -0600

    refactor on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+
+import { Header } from "./components/Header";
 import { Form } from "./components/Form";
 import { ListTasks } from "./components/ListTasks";
+
 import { useRefreshTasks } from "./hooks/useRefreshTasks";
 import "./App.css";
-import { Header } from "./components/Header";
 
 function App() {
   // Custom hooks
   const { refresTasks } = useRefreshTasks();
-
-  // Redux
-  const tasks = useSelector((state) => state.tasks.value);
 
   useEffect(() => {
     refresTasks();
@@ -25,7 +23,7 @@ function App() {
         <Form />
       </section>
 
-      {tasks.length > 0 && <ListTasks />}
+      <ListTasks />
     </main>
   );
 }
